d71a58e83bd9f756d5bb3cc809c0e617447c9073
tunimosmann <rossimosmann@gmail.com>
2019-08-05 15:33:01 -0400

    initial commit! :octocat:

---
+++ bridge-projet1/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Data for testing
+import users from './testData/users';
+import repos from './testData/repos';
+import events from './testData/events';
+import pulls from './testData/pulls';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello world!</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
